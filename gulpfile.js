c4053b4965af9245ee1b7ae83853544660bbdc39
“berkkarahan” <berkkarahan00@gmail.com>
2020-03-26 22:06:49 +0300

    working package conf for gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,100 +1,116 @@
-var browserify = require('browserify');
-var gulp = require('gulp');
-var babelify = require('babelify');
-var source = require('vinyl-source-stream');
+const browserify = require("browserify");
+const gulp = require("gulp");
+const babelify = require("babelify");
+const source = require("vinyl-source-stream");
 
-var destFolder = './bundle/';
-
-
+const destFolder = "./bundle/";
 
 // ---------------------------------------------- Gulp Tasks
-gulp.task('index', function(){
-    return browserify('./src/js/views/indexView.js')
-    .transform(babelify.configure({
-        presets: ["es2015"]
-    }))
+gulp.task("index", function() {
+  return browserify("./src/js/views/indexView.js")
+    .transform(
+      babelify.configure({
+        presets: ["@babel/env"]
+      })
+    )
     .bundle()
-    .pipe(source('indexBundle.js'))
+    .pipe(source("indexBundle.js"))
     .pipe(gulp.dest(destFolder));
 });
 
-gulp.task('signup', function(){
-    return browserify('./src/js/views/signUpView.js')
-    .transform(babelify.configure({
-        presets: ["es2015"]
-    }))
+gulp.task("signup", function() {
+  return browserify("./src/js/views/signUpView.js")
+    .transform(
+      babelify.configure({
+        presets: ["@babel/env"]
+      })
+    )
     .bundle()
-    .pipe(source('signUpBundle.js'))
+    .pipe(source("signUpBundle.js"))
     .pipe(gulp.dest(destFolder));
 });
 
-gulp.task('about', function(){
-    return browserify('./src/js/views/aboutView.js')
-    .transform(babelify.configure({
-        presets: ["es2015"]
-    }))
+gulp.task("about", function() {
+  return browserify("./src/js/views/aboutView.js")
+    .transform(
+      babelify.configure({
+        presets: ["@babel/env"]
+      })
+    )
     .bundle()
-    .pipe(source('aboutBundle.js'))
+    .pipe(source("aboutBundle.js"))
     .pipe(gulp.dest(destFolder));
 });
 
-gulp.task('searchResult', function(){
-    return browserify('./src/js/views/searchResultView.js')
-    .transform(babelify.configure({
-        presets: ["es2015"]
-    }))
+gulp.task("searchResult", function() {
+  return browserify("./src/js/views/searchResultView.js")
+    .transform(
+      babelify.configure({
+        presets: ["@babel/env"]
+      })
+    )
     .bundle()
-    .pipe(source('searchResultBundle.js'))
+    .pipe(source("searchResultBundle.js"))
     .pipe(gulp.dest(destFolder));
 });
 
-gulp.task('search', function(){
-    return browserify('./src/js/views/searchView.js')
-    .transform(babelify.configure({
-        presets: ["es2015"]
-    }))
+gulp.task("search", function() {
+  return browserify("./src/js/views/searchView.js")
+    .transform(
+      babelify.configure({
+        presets: ["@babel/env"]
+      })
+    )
     .bundle()
-    .pipe(source('searchBundle.js'))
+    .pipe(source("searchBundle.js"))
     .pipe(gulp.dest(destFolder));
 });
 
-gulp.task('companyRating', function(){
-    return browserify('./src/js/views/companyRatingView.js')
-    .transform(babelify.configure({
-        presets: ["es2015"]
-    }))
+gulp.task("companyRating", function() {
+  return browserify("./src/js/views/companyRatingView.js")
+    .transform(
+      babelify.configure({
+        presets: ["@babel/env"]
+      })
+    )
     .bundle()
-    .pipe(source('companyRatingBundle.js'))
+    .pipe(source("companyRatingBundle.js"))
     .pipe(gulp.dest(destFolder));
 });
 
-gulp.task('companyComment', function(){
-    return browserify('./src/js/views/companyCommentView.js')
-    .transform(babelify.configure({
-        presets: ["es2015"]
-    }))
+gulp.task("companyComment", function() {
+  return browserify("./src/js/views/companyCommentView.js")
+    .transform(
+      babelify.configure({
+        presets: ["@babel/env"]
+      })
+    )
     .bundle()
-    .pipe(source('companyCommentBundle.js'))
+    .pipe(source("companyCommentBundle.js"))
     .pipe(gulp.dest(destFolder));
 });
 
-gulp.task('companies', function(){
-    return browserify('./src/js/views/companiesView.js')
-    .transform(babelify.configure({
-        presets: ["es2015"]
-    }))
+gulp.task("companies", function() {
+  return browserify("./src/js/views/companiesView.js")
+    .transform(
+      babelify.configure({
+        presets: ["@babel/env"]
+      })
+    )
     .bundle()
-    .pipe(source('companiesBundle.js'))
+    .pipe(source("companiesBundle.js"))
     .pipe(gulp.dest(destFolder));
 });
 
-gulp.task('contact', function(){
-    return browserify('./src/js/views/contactView.js')
-    .transform(babelify.configure({
-        presets: ["es2015"]
-    }))
+gulp.task("contact", function() {
+  return browserify("./src/js/views/contactView.js")
+    .transform(
+      babelify.configure({
+        presets: ["@babel/env"]
+      })
+    )
     .bundle()
-    .pipe(source('contactBundle.js'))
+    .pipe(source("contactBundle.js"))
     .pipe(gulp.dest(destFolder));
 });
 
@@ -102,6 +118,6 @@ gulp.task('contact', function(){
 
 /* gulp.task('default', ['index', 'signup']); */
 
-gulp.task('default', function(){
-    console.log("It's working!");
+gulp.task("default", function() {
+  console.log("It's working!");
 });
